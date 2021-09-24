9369c213418126b359b053d71ad5e434629f40b4
sakshi <sakshiyadav30456@gmail.com>
2021-09-24 13:45:55 +0530

    Uploading all files

---
+++ calculator/src/calculator/allien.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class allien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ calculator/src/calculator/cal.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class cal {
+
+	public int add(int num1,int num2)
+	{
+		int r=num1+num2;
+		return r;
+	}
+
+	
+	public int sub(int num1 ,int num2)
+	
+	{
+		int r=num1-num2;
+		return r;
+	}
+}
